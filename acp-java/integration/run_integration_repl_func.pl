d7fc53d2cecf25f031fb4f887f2d2271985f840d
MinWooJin <alsdn653@naver.com>
2018-12-27 03:33:03 +0900

    FIX: repl_func request miss

--- acp-java/integration/run_integration_repl_func.pl
+++ acp-java/integration/run_integration_repl_func.pl
@@ -118,8 +118,8 @@ if ($flag eq -1 || $flag eq 1) {
       #"single_server=" . $zk_ip . ":" . $t_port . "\n" .
       "client=100\n" .
       "rate=400\n" .
-      "request=-1\n" .
-      "time=$run_time\n" .
+      "request=0\n" .
+      "time=-1\n" .
       "keyset_size=$keyset_size\n" .
       "valueset_min_size=10\n" .
       "valueset_max_size=30\n" .
